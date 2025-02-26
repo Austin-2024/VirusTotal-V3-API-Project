e8449dc9b4e54dedd767afc21a0af3a2427542fa
Austin <austin.schuller09@gmail.com>
2025-02-26 10:27:43 -0600

    Commented the code
    I should have done this from the beginning
    
    Everything is commented, probably too much though, hopefully they are useful comments

--- scanner.go
+++ scanner.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 )
 
+// Struct for saving the body in the url scan section
 type VirusTotalResponse struct {
 	Data struct {
 		Links struct {
@@ -29,6 +30,7 @@ type VirusTotalResponse struct {
 	} `json:"data"`
 }
 
+// Struct for getting the id from file upload section
 type FileScanID struct {
 	Data struct {
 		ID string `json:"id"`
@@ -37,22 +39,30 @@ type FileScanID struct {
 
 func analyzeURL(URL string) {
 
+	// Encodes the user submitted URL into base64. This is done because that's what the VT docs said the format of the url needed to be
 	var urlID = base64.RawURLEncoding.EncodeToString([]byte(URL))
 
+	// URL for requesting the scan results
 	url := "https://www.virustotal.com/api/v3/urls/" + urlID
 
+	// Creates the request to GET the url. The 3rd param is nil because we don't need it for GET requests
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("accept", "application/json")
-	req.Header.Add("x-apikey", "APIKEY")
+	req.Header.Add("x-apikey", "APIKEY") // APIKEY goes here
+
+	// Sends the request
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println("Request failed:", err)
 		return
 	}
+	// Closes the body once the function returns
 	defer res.Body.Close()
+	// Saves the output into a variable called body
 	body, _ := io.ReadAll(res.Body)
 
+	// This commented out part saves the body to a json file. If you wish to save the output to json then uncomment this block
 	/*
 		file, err := os.Create("body.json")
 		if err != nil {
@@ -69,52 +79,73 @@ func analyzeURL(URL string) {
 		}
 	*/
 
+	// Gets the body json from the VirusTotalResponse struct | The struct is towards the top of the program
 	var urlResults VirusTotalResponse
-	err = json.Unmarshal(body, &urlResults)
+	// Checks for errors, also for some reason required. If I leave this part out I can't even get data from it
+	err = json.Unmarshal(body, &urlResults) // Still new to json stuff so idk what this does besides check for errors when reading the json
 	if err != nil {
 		fmt.Println("Error parsing json:", err)
 		return
 	}
 
+	// Outputs the scan results | The only results it shows is the numerical score
+	// How many scanners said the url was Harmless, Malicious, Suspicious, or Undetected
 	fmt.Println("\nScan results for " + URL)
 	fmt.Println("Virus Total URL:", urlResults.Data.Links.Self)
 	fmt.Println("Harmless:", urlResults.Data.Attributes.LastAnalysisStats.Harmless)
 	fmt.Println("Malicious:", urlResults.Data.Attributes.LastAnalysisStats.Malicious)
 	fmt.Println("Suspicious:", urlResults.Data.Attributes.LastAnalysisStats.Suspicious)
 	fmt.Println("Undetected:", urlResults.Data.Attributes.LastAnalysisStats.Undetected)
+	return
 
 }
 
+// This function takes the url, sends it to virustotal and scans it. The function it calls then retrieves the analysis
+// report to be displayed to the user
 func scanURL() {
 	fmt.Print("URL to scan: ")
-	var URL string
+	var URL string // User submitted URL to be scanned
 	fmt.Scan(&URL)
 
+	// This is the v3 api url for sending requests to VirusTotal
 	url := "https://www.virustotal.com/api/v3/urls"
 
+	// For whatever reason this is required to make it work
+	// Whenever I try: payload := strings.NewReader(URL) it gives me an error, but when I do url = URL then it works
 	url = URL
-	payload := strings.NewReader(url)
+	payload := strings.NewReader(url) // This reads the url and sets it as the payload for the VT request
+	// Also the payload is the url the user submits, while url is the V3 api url
 
-	req, _ := http.NewRequest("POST", url, payload)
+	req, _ := http.NewRequest("POST", url, payload) // Creates a http POST request to Virustotal, using the url and the payload.
 
+	// Required headers by VirusTotal | Not completely sure what they do, but the documentation said these were needed
 	req.Header.Add("accept", "application/json")
-	req.Header.Add("x-apikey", "APIKEY")
+	req.Header.Add("x-apikey", "APIKEY") // I do know this one :) it's the apikey header so they can authenticate the API call
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
 
+	// This sends the http request with the headers set above
 	res, err := http.DefaultClient.Do(req)
+	// The error handling isn't "required", but golang prefers me putting this in here
+	// This program has error handling everywhere where golang wants there to be, some of it is probably unneccesary
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
+	// Closes the Body once the function returns
 	defer res.Body.Close()
 
+	// Calls the function for getting the scan analysis, also sends the URL variable
+	// The url variable is the URL that the user submitted
 	analyzeURL(URL)
 	return
 
 }
 
+// Function for retrieving the scan results
+// scanID is the id gotten from uploading the file | It is a []byte type
 func analyzeFile(scanID []byte) {
 
+	// Accessing the FileScanID struct to retrieve the scanID
 	var FileUploadBody FileScanID
 	err := json.Unmarshal(scanID, &FileUploadBody)
 	if err != nil {
@@ -122,12 +153,16 @@ func analyzeFile(scanID []byte) {
 		return
 	}
 
+	// Debug print statement to see if the ID is grabbed
 	fmt.Println("ID2:", FileUploadBody.Data.ID)
 
+	// Testing to see if the urlID needed to be encoded
 	var urlID = base64.RawURLEncoding.EncodeToString([]byte(FileUploadBody.Data.ID))
 
+	// url for api calls
 	url := "https://www.virustotal.com/api/v3/files/" + urlID
 
+	// Creates the request
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("accept", "application/json")
@@ -145,16 +180,23 @@ func analyzeFile(scanID []byte) {
 
 }
 
+// Function for uploading the file to VirusTotal to be scanned
+// The end of this function calls the function that retrieves the report from virustotal
 func scanFile(path string) {
 
+	// Opens the file to be used
 	file, err := os.Open(path)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	// Closes the file once the function finishes
 	defer file.Close()
 
+	// The following multiple lines are for formatting the file in a way
+	// that can be interperetted by the virustotal api url
+	// multipart format
 	var requestBody bytes.Buffer
 	writer := multipart.NewWriter(&requestBody)
 
@@ -176,22 +218,29 @@ func scanFile(path string) {
 		return
 	}
 
+	// url for the api call
 	url := "https://www.virustotal.com/api/v3/files"
 
+	// Creates the request, &requestBody is the formatted file
 	req, _ := http.NewRequest("POST", url, &requestBody)
 
+	// Headers for the request
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("x-apikey", "APIKEY")
 	req.Header.Add("content-type", writer.FormDataContentType())
 
+	// Sends the request
 	results, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 
+	// Close the body once the function returns
 	defer results.Body.Close()
+	// Saves the body to a variable
 	body, _ := io.ReadAll(results.Body)
 
+	// This is for debugging because I'm having issues getting the scan results back
 	var test FileScanID
 	err = json.Unmarshal(body, &test)
 	if err != nil {
@@ -201,6 +250,7 @@ func scanFile(path string) {
 
 	fmt.Println("ID:", test.Data.ID)
 
+	// Calls the function for retrieving the scan results
 	analyzeFile(body)
 
 }
@@ -240,6 +290,7 @@ func pickFile() {
 
 func main() {
 
+	// Just a little intro
 	fmt.Println("VirusTotal v3 API URL and File scanner")
 	fmt.Println("Made by Austin, using Golang")
 	fmt.Print("\n")
@@ -247,15 +298,16 @@ func main() {
 	fmt.Print("Option: ")
 
 	var scanOption int
-	fmt.Scan(&scanOption)
+	fmt.Scan(&scanOption) // Scan gets input from the last line, &scanOption puts the input into the scanOption variable
 
+	// Function for scanning URL
 	if scanOption == 1 {
 		scanURL()
-	} else if scanOption == 2 {
+	} else if scanOption == 2 { // Function for scanning file
 		pickFile()
 	} else {
 		fmt.Println("\nYou selected an invalid option, please select either 1 or 2.")
-		main()
+		main() // If the user doesn't choose 1 or 2
 	}
 
 }
